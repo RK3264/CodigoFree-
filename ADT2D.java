a5020831222bbaf127456ddd7828dfe76c53af88
RK3264 <113079537+RK3264@users.noreply.github.com>
2022-09-07 22:00:18 -0500

    Update ADT2D.java

--- ADT2D.java
+++ ADT2D.java
@@ -1,17 +1,11 @@
-
 import java.util.ArrayList;
-
 public class ADT2D<E> {
-
     private ArrayList<E> ren, col, aux;
     private int numRenglones, numColumnas;
     
-    
-    public ADT2D(int a, int b){
-        
+    public ADT2D(int a, int b){    
         this.numRenglones = a;
         this.numColumnas = b;
-        
         for(int i=0; i<numRenglones ; i++){
             ArrayList<E> tmp = new ArrayList();
             for(int j=0 ; j<numColumnas ; j++){
@@ -20,15 +14,6 @@ public class ADT2D<E> {
             ren.add((E) tmp);   
         }
     }
-    
-    public int getLenghtRow(){
-        return this.numRenglones;
-    }
-    
-    public int getLenghtCol(){
-        return this.numColumnas;
-    }
-    
     public void clear(E dato){
         for(int i=0; i<numRenglones ; i++){
             for(int j=0 ; j<numColumnas ; j++){
@@ -37,19 +22,22 @@ public class ADT2D<E> {
             ren.set(i, (E) col);
         }
     }
-    
     public void setElement(int a,int b,E dato){
             aux =(ArrayList<E>) ren.get(a);
             System.out.println(aux);
             aux.set(b, dato);          
             ren.set(a, (E) aux);    
     }
-    
     public E getElement(int a, int b){
         aux = (ArrayList<E>)ren.get(a);
         return (E) aux.get(b);            
     } 
-    
+    public int getLenghtRow(){
+        return this.numRenglones;
+    }
+    public int getLenghtCol(){
+        return this.numColumnas;
+    }
     public String toString(){
         return String.format("%s", ren);
     } 
