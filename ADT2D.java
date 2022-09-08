5b94f9967494e2b0fcb1b0634f848e012cacf655
RK3264 <113079537+RK3264@users.noreply.github.com>
2022-09-07 21:58:01 -0500

    Arreglo 2D
    ;)

---
+++ ADT2D.java
@@ -0,0 +1,57 @@
+package com.rk.estructurasdedatos;
+
+import java.util.ArrayList;
+
+public class ADT2D<E> {
+
+    private ArrayList<E> ren, col, aux;
+    private int numRenglones, numColumnas;
+    
+    
+    public ADT2D(int a, int b){
+        
+        this.numRenglones = a;
+        this.numColumnas = b;
+        
+        for(int i=0; i<numRenglones ; i++){
+            ArrayList<E> tmp = new ArrayList();
+            for(int j=0 ; j<numColumnas ; j++){
+                tmp.add(null);
+            }
+            ren.add((E) tmp);   
+        }
+    }
+    
+    public int getLenghtRow(){
+        return this.numRenglones;
+    }
+    
+    public int getLenghtCol(){
+        return this.numColumnas;
+    }
+    
+    public void clear(E dato){
+        for(int i=0; i<numRenglones ; i++){
+            for(int j=0 ; j<numColumnas ; j++){
+                col.set(j,dato);
+            }
+            ren.set(i, (E) col);
+        }
+    }
+    
+    public void setElement(int a,int b,E dato){
+            aux =(ArrayList<E>) ren.get(a);
+            System.out.println(aux);
+            aux.set(b, dato);          
+            ren.set(a, (E) aux);    
+    }
+    
+    public E getElement(int a, int b){
+        aux = (ArrayList<E>)ren.get(a);
+        return (E) aux.get(b);            
+    } 
+    
+    public String toString(){
+        return String.format("%s", ren);
+    } 
+}
